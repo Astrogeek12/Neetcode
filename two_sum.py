6d2128f1e3efabf518825db0d0695218be2f55c3
Astrogeek12 <109037595+Astrogeek12@users.noreply.github.com>
2024-01-04 16:05:18 +0530

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,1 +1,9 @@
-d
+def two_sum(nums,target):
+  seen={}
+  for i , nums in enumerate(nums):
+    complement=target-nums
+
+    if complement in seen:
+      return [seen[complement],i]
+      seen[num]=i
+  return None
